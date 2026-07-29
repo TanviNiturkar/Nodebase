78a6446595a10b878b5c5b1ee70b5b1694dc61cd
Tanvi Niturkar <tanvipniturkar93@gmail.com>
2026-07-29 06:15:33 +0530

    Update page.tsx

--- src/app/(dashboard)/(rest)/credentials/[credentialId]/page.tsx
+++ src/app/(dashboard)/(rest)/credentials/[credentialId]/page.tsx
@@ -18,7 +18,7 @@ const {credentialId} = await params;
 prefetchCredential(credentialId)
         return (
             <div className="p-4 md:px-10 md:py-6 h-full">
-                <div className="mx-auto mx-w-screen-md w-full flex flex-col gap-y-8 h-full">
+                <div className="mx-auto max-w-screen-md w-full flex flex-col gap-y-8 h-full">
                   <HydrateClient>
                     <ErrorBoundary fallback={<CredentialsError />}>
                     <Suspense fallback={<CredentialsLoading />}>
