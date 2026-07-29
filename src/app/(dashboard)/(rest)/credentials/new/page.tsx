8946f286e99a09e5cf0053c230d12f776b5c0159
Tanvi Niturkar <tanvipniturkar93@gmail.com>
2026-07-29 06:16:08 +0530

    Fix typo in class name for layout div

--- src/app/(dashboard)/(rest)/credentials/new/page.tsx
+++ src/app/(dashboard)/(rest)/credentials/new/page.tsx
@@ -8,7 +8,7 @@ const Page =async () =>{
     await requireAuth()
     return(
         <div className="p-4 md:px-10 md:py-6 h-full">
-            <div className="mx-auto mx-w-screen-md w-full flex flex-col gap-y-8 h-full">
+            <div className="mx-auto max-w-screen-md w-full flex flex-col gap-y-8 h-full">
                 <CredentialForm />
             </div>
         </div>
