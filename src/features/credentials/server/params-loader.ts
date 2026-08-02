93f05ececfbf218af470d53cbbc05429e0e10fa3
Tanvi Niturkar <tanvipniturkar93@gmail.com>
2026-08-03 05:24:30 +0530

    Update credentialsParamsLoader to use credentialsParams

--- src/features/credentials/server/params-loader.ts
+++ src/features/credentials/server/params-loader.ts
@@ -1,6 +1,6 @@
-import { workflowsParams } from "@/features/workflows/params";
+import { credentialsParams } from "@/features/credentials/params";
 import { createLoader } from "nuqs/server";
 
 
 
-export const credentialsParamsLoader = createLoader(workflowsParams)
+export const credentialsParamsLoader = createLoader(credentialsParams)
